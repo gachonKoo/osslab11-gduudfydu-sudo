0021aad5df6127b1a98c51033489b083044e8a13
gduudfydu-sudo <gduudfydu@gmail.com>
2025-11-15 23:34:01 +0900

    Use utility functions for calculations

--- tester.py
+++ tester.py
@@ -1,8 +1,9 @@
 import geo.utils as utils
-a, b = 3, 4
-c= 
-print('c=', c)
 
-r=10
-area= 
-print('area=', area)
+a, b = 3, 4
+c = utils.pythagoras(a, b)
+print('c =', c)
+
+r = 10
+area = utils.circle(r)
+print('area =', area)
